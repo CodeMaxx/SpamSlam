90d68f97e01db08b04bb91df56f1ba0052bd6c33
Kumar Ayush <cheekujodhpur@gmail.com>
2017-10-07 23:30:42 +0530

    added models

--- spamslam/website/admin.py
+++ spamslam/website/admin.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from website.models import *
 # Register your models here.
+
+class EmailAdmin(admin.ModelAdmin):
+	pass
+
+
+class TransactionAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Email, EmailAdmin)
+admin.site.register(Transaction, TransactionAdmin)
