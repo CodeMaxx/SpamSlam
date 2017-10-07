f52bd777d4d55ebac6091aa98f5a389efcc106b8
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-10-07 23:22:28 +0530

    changes added

--- spamslam/website/views.py
+++ spamslam/website/views.py
@@ -7,8 +7,15 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from website.models import *
+from predict import predict_from_text
 
 # Create your views here.
 
 def home(request):
 	return render(request, 'homepage.html', {})
+
+def check_spam(input_sentence):
+	f = open('list.txt', 'w')
+	f.write(input_sentence)
+	f.close()
+	prediction = predict_from_text()
